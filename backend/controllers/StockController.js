c153b8e994fb94e01503101f37e4b1ddf3a18c69
1shChheda <1shvenom786@gmail.com>
2024-03-31 00:12:42 +0530

    getAllStocks for explore page

--- backend/controllers/StockController.js
+++ backend/controllers/StockController.js
@@ -4,52 +4,69 @@ const csv = require('csv-parser');
 const { Stock } = require('../models/Stock');
 
 const addStocks = async (req, res, next) => {
-    try {
-        const data = [];
+  try {
+    const data = [];
 
-        fs.createReadStream('C:/Users/Admin/Downloads/AAPL.csv')
-          .pipe(csv())
-          .on('data', (row) => {
-            // extracting data from the CSV row and push it into the array
-            data.push({
-              date: new Date(row.date),
-              open: parseFloat(row.open),
-              high: parseFloat(row.high),
-              low: parseFloat(row.low),
-              close: parseFloat(row.close),
-              adjClose: parseFloat(row.adjClose),
-              volume: parseInt(row.volume)
-            });
-          })
-          .on('end', async () => {
-            try {
+    fs.createReadStream('C:/Users/Admin/Downloads/AAPL.csv')
+      .pipe(csv())
+      .on('data', (row) => {
+        // extracting data from the CSV row and push it into the array
+        data.push({
+          date: new Date(row.date),
+          open: parseFloat(row.open),
+          high: parseFloat(row.high),
+          low: parseFloat(row.low),
+          close: parseFloat(row.close),
+          adjClose: parseFloat(row.adjClose),
+          volume: parseInt(row.volume)
+        });
+      })
+      .on('end', async () => {
+        try {
 
-              // find or create the stock document
-              let stock = await Stock.findOne({ shortName: 'AAPL' });
-              if (!stock) {
-                stock = await Stock.create({ shortName: 'AAPL', fullName: "Apple Inc.", data: [] });
-              }
-              
-              // appendin the data array to the existing data in the stock document
-              stock.data = [...stock.data, ...data];
-              await stock.save();
+          // find or create the stock document
+          let stock = await Stock.findOne({ shortName: 'AAPL' });
+          if (!stock) {
+            stock = await Stock.create({ shortName: 'AAPL', fullName: "Apple Inc.", data: [] });
+          }
 
-              console.log('Stock data saved successfully');
-              console.log('CSV file successfully processed');
+          // appendin the data array to the existing data in the stock document
+          stock.data = [...stock.data, ...data];
+          await stock.save();
 
-              res.status(200).json({ message: 'Stock data saved successfully' });
-            } catch (error) {
-              console.error('Error saving stock data:', error);
-              res.status(500).json({ message: 'Internal Server Error 2 ' });
-            }
-          });
+          console.log('Stock data saved successfully');
+          console.log('CSV file successfully processed');
 
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: "Internal Server Error 1" });
+          res.status(200).json({ message: 'Stock data saved successfully' });
+        } catch (error) {
+          console.error('Error saving stock data:', error);
+          res.status(500).json({ message: 'Internal Server Error 2 ' });
+        }
+      });
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error 1" });
+  }
+}
+
+const getAllStocks = async (req, res, next) => {
+  try {
+    const stocks = await Stock.find();
+
+    if (!stocks || stocks.length === 0) {
+      return res.status(404).json({ message: "Stocks data Unavailable" });
     }
+
+    return res.status(200).json(stocks);
+    
+  } catch (error) {
+    console.error('Error getting Stock data:', error);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
 }
 
 module.exports = {
-    addStocks,
+  addStocks,
+  getAllStocks,
 }
